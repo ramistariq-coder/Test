107518b355c5cd244f82847801677febfb398a76
ramistariq-coder <ramistariq@gmail.com>
2026-03-11 17:04:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 </> JavaScript 
 const express = require("express");
+const axios = require("axios");
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("POS API Running 🚀");
+});
+
+app.post("/pos-sale", async (req, res) => {
+  const { phone, amount } = req.body;
+
+  console.log("POS Sale:", name, phone, amount);
+
+  res.json({ status: "ok" });
 });
 
 const PORT = process.env.PORT || 3000;

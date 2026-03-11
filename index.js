0a8bbef6c61234d1536098d47a07c70e1475ddc0
ramistariq-coder <ramistariq@gmail.com>
2026-03-11 12:40:12 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+</> JavaScript 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("POS API Running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
